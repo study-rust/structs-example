729bc8bf7192bde9eb368ab0f315a6625eb9bf12
wooklym <wooklym@gmail.com>
2019-03-06 00:10:37 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn from(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle::from(30, 50);
+    let rect2 = Rectangle::from(20, 10);
+    let rect3 = Rectangle::from(10, 60);
+
+    println!("area: {}", rect1.area());
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("{:#?}", rect1);
+}
